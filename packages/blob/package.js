54016833206e3de62fc1eacdd3b95be3098ed187
Mitar <mitar.git@tnode.com>
2013-10-24 00:16:02 -0700

    Added comment for the blob package.

--- packages/blob/package.js
+++ packages/blob/package.js
@@ -1,3 +1,11 @@
+// We are using this package only to polyfill Blob implementation on
+// PhantomJS which is used in our Travis CI tests. PhantomJS uses
+// internally an old WebKit implementation which has some bugs.
+//
+// See:
+// https://github.com/peerlibrary/peerlibrary/issues/157
+// https://github.com/ariya/phantomjs/issues/11013
+
 Package.describe({
   summary: "Polyfill for Blob"
 });
