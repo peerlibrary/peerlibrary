002ee8f21c20f410377b156588cefe6f40f3518a
Mitar <mitar.git@tnode.com>
2014-01-11 21:00:32 -0800

    Style.

--- client/js/jquery.forwardMouseEvents.js
+++ client/js/jquery.forwardMouseEvents.js
@@ -31,11 +31,9 @@
             $lastT = null;
 
         $this.on('mouseleave', function (e) {
-            if ($lastT) {
-                if (e.relatedTarget !== $lastT[0]) {
-                    trigger($lastT, 'mouseout', e, e.relatedTarget);
-                    $lastT = null;
-                }
+            if ($lastT && e.relatedTarget !== $lastT[0]) {
+                trigger($lastT, 'mouseout', e, e.relatedTarget);
+                $lastT = null;
             }
         }).on('mousemove mousedown mouseup mousewheel click dblclick', function (e) {
             if (!$this.is(':visible'))
