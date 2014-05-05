a7913733f1fe6b49eb08bb1fe76e6f2604be467a
Marin Pranjić <marin.pranjic@gmail.com>
2014-05-05 13:22:19 +0200

    Minor cleanup in sha256 worker.

--- packages/sha256/web-worker.js
+++ packages/sha256/web-worker.js
@@ -1,39 +1,31 @@
 importScripts('digest.js/digest.js');
 
-onmessage = function(oEvent){
+onmessage = function (oEvent){
   var message = oEvent.data.message;
-  ChunkHandler[ message ](oEvent.data);
+  ActionHandler[message](oEvent.data);
 }
 
-function SHA256WebWorker_getFinalHashString(hash) {
-  var hexTab = '0123456789abcdef', sha256 = '', _i, _len, a;
-  var array = new Uint8Array(hash.finalize());
-  for (_i = 0, _len = array.length; _i < _len; _i++) {
-    a = array[_i];
-    sha256 += hexTab.charAt((a >>> 4) & 0xF) + hexTab.charAt(a & 0xF);
-  }
-  return sha256;
-}
-
-var ChunkHandler = {
+// handles messages received from main thread
+var ActionHandler = {
   hash: Digest.SHA256(),
-  chunk: function SHA256WebWorkerChunkHandler_updateChunk(eventData) {
+  chunk: function SHA256WebWorkerChunkHandler_updateChunk (eventData) {
     // TODO: Handle chunkNumber
     var chunk = eventData.chunk;
     this.hash.update(chunk);
   },
-  finalize: function SHA256WebWorkerChunkHandler_finalizeChunk(eventData) {
+  finalize: function SHA256WebWorkerChunkHandler_finalizeChunks (eventData) {
     MessageHandler.done(SHA256WebWorker_getFinalHashString(this.hash));
   },
-  file: function SHA256WebWorkerChunkHandler_processFile(eventData) {
+  file: function SHA256WebWorkerChunkHandler_processFile (eventData) {
     var file = eventData.file;
     var chunkSize = eventData.chunkSize;
     SHA256WebWorker_getSHA256FromFile(file, chunkSize);
   }
 }
 
+// structures and sends messages to main thread
 var MessageHandler = {
-  progress: function SHA256WebWorkerMessageHandler_sendChunkInfo(chunkNumber, chunkSize, fileSize){
+  progress: function SHA256WebWorkerMessageHandler_sendChunkInfo (chunkNumber, chunkSize, fileSize){
     postMessage({
       message: 'progress',
       data: {
@@ -42,7 +34,7 @@ var MessageHandler = {
       }
     });
   },
-  done: function SHA256WebWorkerMessageHandler_sendSHA256(sha256){
+  done: function SHA256WebWorkerMessageHandler_sendSHA256 (sha256){
     postMessage({
       message: 'done',
       data: {
@@ -52,7 +44,19 @@ var MessageHandler = {
   }
 }
 
-function SHA256WebWorker_getSHA256FromFile(file, chunkSize){
+// converts binary hash to hex (string) representation
+function SHA256WebWorker_getFinalHashString (hash) {
+  var hexTab = '0123456789abcdef', sha256 = '', _i, _len, a;
+  var array = new Uint8Array(hash.finalize());
+  for (_i = 0, _len = array.length; _i < _len; _i++) {
+    a = array[_i];
+    sha256 += hexTab.charAt((a >>> 4) & 0xF) + hexTab.charAt(a & 0xF);
+  }
+  return sha256;
+}
+
+// calculates hash from File/Blob object but in chunks to reduce memory footprint
+function SHA256WebWorker_getSHA256FromFile (file, chunkSize){
   var fileSize = file.size;
   var reader = new FileReaderSync();
   var hash = Digest.SHA256();
--- packages/sha256/worker.coffee
+++ packages/sha256/worker.coffee
@@ -3,26 +3,24 @@ SHA256Worker =
   disableWorker: false
   workerSrc: '/packages/sha256/web-worker.js'
   chunkSize: 1024 * 32 # bytes
-  chunks: false
+  _chunks: false
 
   fromFile: (params) ->
-    if @chunks
+    if @_chunks
       throw new Error('Unable to receive file while receiving chunks')
     @initWorker params
     @worker.sendFile params
 
   addChunk: (params) ->
-    if not @chunks
-      console.log "Processing file in chunks"
+    if not @_chunks
       @initWorker params
-      @chunks = true
+      @_chunks = true
     @worker.sendChunk params
 
   finalize: ->
-    if not @chunks
+    if not @_chunks
       throw new Error('Unable to finalize - no chunks sent')
-    @worker.sendFinalize()
-    console.log "Final chunk received" 
+    @worker.finalize()
 
   initWorker: (params) ->
     if !@disableWorker && window && window.Worker
@@ -62,7 +60,7 @@ class WebWorker extends BaseWorker
   sendChunk: (params) ->
     @worker.postMessage message: 'chunk', chunk: params.chunk
 
-  sendFinalize: (params) ->
+  finalize: (params) ->
     @worker.postMessage message: 'finalize'
 
 class WorkerFallback extends BaseWorker
@@ -71,7 +69,7 @@ class WorkerFallback extends BaseWorker
 
     @_chunkSize = SHA256Worker.chunkSize
     @_chunkStart = 0
-    @chunks = false
+    @_chunks = false
     
   sendFile: (params) ->
     self = @
@@ -81,7 +79,6 @@ class WorkerFallback extends BaseWorker
     @_hash = new Crypto.SHA256()
     @_reader.onload = ->
       self._hash.update @result
-      console.log self._hash
       self._handler.progress chunkNumber: @_chunkStart / @_chunkSize, progress: Math.min(@_chunkStart, @_fileSize) / @_fileSize
       self.readNext()
     self.readNext()
@@ -92,16 +89,15 @@ class WorkerFallback extends BaseWorker
     end = start + @_chunkSize
     # check if all the chunks are read
     if start >= @_fileSize
-      @sendFinalize()
+      @finalize()
     else
       # increase chunkStart
       @_chunkStart = end
-      console.log @_file
       blob = @_file.slice start, end
       @_reader.readAsArrayBuffer blob
     return
 
-  sendFinalize: ->
+  finalize: ->
     sha256 = @_hash.finalize()
     @_handler.done sha256: sha256   
   
