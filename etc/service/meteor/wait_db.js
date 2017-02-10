776ff443fe0627cb848f10e709c2391e3967a633
Mitar <mitar.git@tnode.com>
2017-02-10 00:00:29 -0800

    Updating waiting script.

--- etc/service/meteor/wait_db.js
+++ etc/service/meteor/wait_db.js
@@ -3,19 +3,32 @@ var mongodb = require('mongodb');
 var waitingFor = 2;
 
 function tryConnect(url) {
-  mongodb.MongoClient.connect(url, function (error, db) {
+  mongodb.MongoClient.connect(url, {
+      server: {
+        socketOptions: {
+          connectTimeoutMS: 5000,
+          socketTimeoutMS: 5000
+        }
+      }
+    }, function (error, db) {
     if (error === null) {
-      db.command({ping:1}, function(error, result) {
+      db.command({ping: 1}, function(error, result) {
         if (error === null) {
           if (--waitingFor <= 0) {
             process.exit(0);
           }
           return;
         }
+        else {
+          console.error("Waiting for database", error);
+        }
 
         setTimeout(function() { tryConnect(url) }, 100);
       });
       return;
+    }
+    else {
+      console.error("Waiting for database", error);
     }
 
     setTimeout(function() { tryConnect(url) }, 100);
@@ -24,3 +37,4 @@ function tryConnect(url) {
 
 tryConnect(process.env.MONGO_URL);
 tryConnect(process.env.MONGO_OPLOG_URL);
+
